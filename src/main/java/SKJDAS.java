09c4526843dea43457ca025b00ef253201e845b0
Yū <artembezzzz@gmail.com>
2024-12-10 00:19:21 +0100

    broadcast implemented, todo: documentation

--- src/main/java/SKJDAS.java
+++ src/main/java/SKJDAS.java
@@ -1,11 +1,9 @@
 import java.io.IOException;
 import java.math.BigInteger;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 
 public class SKJDAS {
     public static void main(String[] args) {
@@ -20,19 +18,19 @@ public class SKJDAS {
         try {
             port = Integer.parseInt(args[0]);
             number = Integer.parseInt(args[1]);
-        } catch (NumberFormatException _) {
+        } catch (NumberFormatException ignored) {
             throw new RuntimeException("usage: SKJDAS <int port> <int number>");
         }
 
         System.out.println("Starting a program, port: " + port + ", number: " + number);
 
-        DatagramPacket packet = null;
+        DatagramPacket packet;
         DatagramSocket socket = null;
 
         try {
             socket = new DatagramSocket(port);
             System.out.println("Entering master mode");
-        } catch (SocketException _) {
+        } catch (SocketException ignored) {
             masterMode = false;
             System.out.println("Entering slave mode");
         }
@@ -40,7 +38,7 @@ public class SKJDAS {
         if (masterMode) {
             ArrayList<Integer> mem = new ArrayList<>();
 
-            packet = new DatagramPacket(new byte[4],4);
+            packet = new DatagramPacket(new byte[4], 4);
             while (true) {
                 try {
                     socket.receive(packet);
@@ -48,30 +46,30 @@ public class SKJDAS {
 
                     while (data[0] == 0 && data.length > 1) {
                         byte[] buf = data;
-                        data = new byte[buf.length-1];
+                        data = new byte[buf.length - 1];
 
                         for (int i = 0; i < data.length; i++) {
-                            data[i] = buf[i+1];
+                            data[i] = buf[i + 1];
                         }
                     }
 
                     int val = new BigInteger(data).intValueExact();
-                    System.out.println(Arrays.toString(data));
+
                     if (val == 0) {
                         mem.add(number);
                         int avg = getAverage(mem);
-                        System.out.println("Average: "  + avg);
-                        //todo broadcast
-                    } else if (val == -1 ) {
+                        System.out.println("Average: " + avg);
+                        broadcast(socket,BigInteger.valueOf(avg).toByteArray(),port);
+                    } else if (val == -1) {
                         System.out.println(val);
-                        //todo broadcast
+                        broadcast(socket,BigInteger.valueOf(val).toByteArray(),port);
                         socket.close();
                         break;
                     } else {
-                        System.out.println(val);
+                        System.out.println("Received: " + val);
                         mem.add(val);
                     }
-                } catch (IOException _) {
+                } catch (IOException ignored) {
                     System.out.println("IOException while receiving a message");
                 }
             }
@@ -91,7 +89,7 @@ public class SKJDAS {
                 data = new byte[4];
 
                 for (int i = 0; i < buf.length; i++) {
-                    data[data.length-1-i] = buf[i];
+                    data[data.length - 1 - i] = buf[i];
                 }
             }
 
@@ -108,13 +106,38 @@ public class SKJDAS {
         System.out.println("Exited");
     }
 
-    public static int getAverage (ArrayList<Integer> list) {
+    public static int getAverage(ArrayList<Integer> list) {
         int avg = 0;
 
         for (Integer i : list) {
             avg += i;
         }
 
-        return avg/list.size();
+        return avg / list.size();
+    }
+
+    private static void broadcast(DatagramSocket socket, byte[] msg, int port) {
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+                if (networkInterface.isLoopback()) {
+                    continue;
+                }
+                for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+                    InetAddress broadcast = interfaceAddress.getBroadcast();
+                    if (broadcast == null) {
+                        continue;
+                    }
+                    DatagramPacket packet = new DatagramPacket(new byte[4],4);
+                    packet.setData(msg);
+                    packet.setPort(port);
+                    packet.setAddress(broadcast);
+                    socket.send(packet);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Could not broadcast a message");
+        }
     }
 }
